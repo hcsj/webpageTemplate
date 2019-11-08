eaa1ed9eb3a702abb4bf91884cef0d0e4e555a0a
sunwenxu1997 <1291397693@qq.com>
2019-11-08 13:11:09 +0800

    完善轮播图文字动画

--- src/components/pc/Home.vue
+++ src/components/pc/Home.vue
@@ -6,7 +6,7 @@
           <div class="swiper-slide" v-for="(i,index) in list" :key="index">
             <div class="bck">
               <span class="text">
-                <span id="text-anime" v-for="(t,indexT) in textCut(i.text)" :key="indexT">{{t}}</span>
+                <span class="text-anime"  v-for="(t,indexT) in textCut(i.text)" :key="indexT">{{t}}</span>
               </span>
             </div>
           </div>
@@ -23,6 +23,7 @@ export default {
   name: "Home",
   data() {
     return {
+      realIndex: 0,
       list: [
         {
           text: "BANNER1"
@@ -40,15 +41,17 @@ export default {
   mounted() {
     let _this = this;
     var banner = document.getElementById("banner");
-    var bck = document.querySelectorAll('.bck')
-    console.log(bck)
+    var bck = document.querySelectorAll(".bck");
+    // var text = document.querySelectorAll(".text-anime");
+    var slide;
     var bannerIndex = 0;
     banner.style.height = _this.windowH + "px";
     var mySwiper = new Swiper(".swiper-container", {
       observer: true, //修改swiper自己或子元素时，自动初始化swiper
       observeParents: true, //修改swiper的父元素时，自动初始化swiper
+      effect: "fade", //切换效果
       followFinger: false,
-      loop: true,
+      speed: 800, //切换间隔时间
       autoplay: true, //开启自动播放
       //显示底部小圆点
       pagination: {
@@ -58,12 +61,34 @@ export default {
         delay: 5000 //4秒切换一次
       },
       on: {
-        slideChangeTransitionStart: function() {
-         console.log(this.realIndex)
-         _this.$anime({
-           targets:`${bck[this.realIndex]}`,
-           translateY:[-200,0]
-         })
+        //初始化
+        init: function(swiper) {
+          bck[0].classList.add("bck-end");
+          let text = bck[0].querySelectorAll(".text-anime");
+          for (let i = 0; i < text.length; i++) {
+            _this.$anime({
+              targets: text[i],
+              delay: i * 100,
+              translateY: [-100, 0]
+            });
+          }
+        },
+        //滑动前移除样式
+        transitionStart: function() {
+          bck[this.realIndex].classList.remove("bck-end");
+        },
+        //滑动结束后添加样式，执行动画
+        transitionEnd: function() {
+          console.log(this.realIndex);
+          bck[this.realIndex].classList.add("bck-end");
+          let text = bck[this.realIndex].querySelectorAll(".text-anime");
+          for (let i = 0; i < text.length; i++) {
+            _this.$anime({
+              targets: text[i],
+              delay: i * 100,
+              translateY: [-100, 0],
+            });
+          }
         }
       }
     });
@@ -92,7 +117,6 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
-    background: #4070f1;
     --swiper-theme-color: white;
     .swiper-slide {
       .bck {
@@ -103,18 +127,17 @@ export default {
         font-size: 3rem;
         color: white;
         letter-spacing: 0.3rem;
-      }
-      #text-anime {
-        display: inline-block;
-        transition: 0.5s;
-        animation: move 0.5s linear both 1;
-      }
-      @keyframes move {
-        0% {
-          transform: translateY(-200px);
+        background: #4070f1;
+        .text-anime {
+          transition: opacity 0s;
+          display: inline-block;
+          opacity: 0;
         }
-        100% {
-          transform: translateY(0px);
+      }
+      .bck-end {
+        .text-anime {
+          transition: opacity 1s;
+          opacity: 1;
         }
       }
     }
